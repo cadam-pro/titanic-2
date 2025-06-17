7f7b94a616ad3405fa6ae676453ba7416524e980
Corentin Adam <cor.adam@laposte.net>
2025-06-17 15:46:53 +0000

    [UPDATE] comment useless stuff again

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import pandas as pd
-from titanic.data import load_data, prepare_data, upload_data
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import pandas as pd
+# from titanic.data import load_data, prepare_data, upload_data
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 import requests
 
 st.title("Welcome to the Streamlit App")
@@ -17,51 +17,51 @@ st.markdown("""
 """)
 
 
-amount = st.slider("Select a number", min_value=0, max_value=100, value=50)
-if st.button("Click Me!") : 
-    params = {"amount": amount}
-    answer = requests.get(url = f"http://127.0.0.1:8000/love", params=params)
-    if answer.status_code == 200:
-        st.success(f"❤️ {answer.json()['love']}")
+# amount = st.slider("Select a number", min_value=0, max_value=100, value=50)
+# if st.button("Click Me!") : 
+#     params = {"amount": amount}
+#     answer = requests.get(url = f"http://127.0.0.1:8000/love", params=params)
+#     if answer.status_code == 200:
+#         st.success(f"❤️ {answer.json()['love']}")
 
-"### Add more functionality as needed."
-" # Dataframe Example"
+# "### Add more functionality as needed."
+# " # Dataframe Example"
 
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [24, 30, 22],
-    'City': ['New York', 'Los Angeles', 'Chicago']
-}
-df = pd.DataFrame(data)
-st.dataframe(df)
+# data = {
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [24, 30, 22],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# }
+# df = pd.DataFrame(data)
+# st.dataframe(df)
 
-@st.cache_data
-def load_data_app():
-    """
-    Load the Titanic dataset.
+# @st.cache_data
+# def load_data_app():
+#     """
+#     Load the Titanic dataset.
     
-    Returns:
-        pd.DataFrame: The Titanic dataset.
-    """
-    print("Dataframe loaded successfully!")
-    return load_data()
+#     Returns:
+#         pd.DataFrame: The Titanic dataset.
+#     """
+#     print("Dataframe loaded successfully!")
+#     return load_data()
 
-df = load_data_app()
+# df = load_data_app()
 
-st.write("### Seaborn")
-f = plt.figure(figsize=(10, 6))
-sns.histplot(df['Age'], kde=True, bins=30)
-st.pyplot(f)
+# st.write("### Seaborn")
+# f = plt.figure(figsize=(10, 6))
+# sns.histplot(df['Age'], kde=True, bins=30)
+# st.pyplot(f)
 
-st.write("### Matplotlib")
-fig, ax = plt.subplots()
-ax.plot(df['Age'], df['Fare'], 'o')
-st.pyplot(fig)
+# st.write("### Matplotlib")
+# fig, ax = plt.subplots()
+# ax.plot(df['Age'], df['Fare'], 'o')
+# st.pyplot(fig)
 
-st.write('Plotly Express')
-import plotly.express as px
-fig = px.scatter(df, x='Age', y='Fare', color='Survived', title='Age vs Fare')
-st.plotly_chart(fig)
+# st.write('Plotly Express')
+# import plotly.express as px
+# fig = px.scatter(df, x='Age', y='Fare', color='Survived', title='Age vs Fare')
+# st.plotly_chart(fig)
 
 
 
